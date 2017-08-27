2dcee5e4050f8e77f4952687c7fbf12bdd2e1274
Dave Duran <dave@yodata.io>
2017-08-26 18:04:14 -0700

    first exports for standardjs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-import Context from './context'
-import {defaultProps, flow, getIn} from './helpers/index';
+import Context from "./context";
+import { defaultProps, flow, getIn } from "./helpers/index";
 
-export default {
-  Context,
-  defaultProps,
-  flow,
-  getIn
-}
+export default Context;
+export { defaultProps, flow, getIn };
